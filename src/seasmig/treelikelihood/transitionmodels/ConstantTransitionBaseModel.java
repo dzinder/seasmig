f812d23b8e5d6e7ec4f51abf48066296cb8f31cb
dzinder <dzinder@EE-ZINDER-DELL.hullbiolabs.uchicago.edu>
2015-03-10 00:40:26 -0500

    m

--- src/seasmig/treelikelihood/transitionmodels/ConstantTransitionBaseModel.java
+++ src/seasmig/treelikelihood/transitionmodels/ConstantTransitionBaseModel.java
@@ -188,7 +188,7 @@ public class ConstantTransitionBaseModel implements TransitionModel {
 		double lambda = -Q[from][from];
 		double time = cern.jet.random.Exponential.staticNextDouble(lambda); // TODO: ??????????
 		// mean of this exponential is 1/lambda, higher the rate, the shorter the time interval --> nextDouble(lambda) is the correct direction.
-		//System.err.println("lambda: "+String.format("%.3f", lambda)+" interval: "+String.format("%.3f", time)+" Q:"+Util.parse(Q));
+		// System.err.println("lambda: "+String.format("%.3f", lambda)+" interval: "+String.format("%.3f", time)+" Q:"+Util.parse(Q));
 		
 		int first = (from+1)%dimension;
 		double p=-Q[from][first]/Q[from][from];
